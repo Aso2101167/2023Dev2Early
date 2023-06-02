d9e89feadfc81a0709a927ae3c968bbfbbec838c
Kakeru1011 <2101167@s.asojuku.ac.jp>
2023-06-02 12:35:51 +0900

    dbmanagerに作成するメソッドを記入

--- 04_ソースコード/php/DBmanager.php
+++ 04_ソースコード/php/DBmanager.php
@@ -1,3 +1,62 @@
 <?php 
     //データベースを操作するファイル
+    class DBManager {
+        //データベース接続のメソッド
+        private function dbConnect(){
+            $pdo = new PDO('mysql:host=;dbname=;charset=utf8',
+							'', '');
+		    return $pdo;
+        }
+
+        //ユーザー新規登録するメソッド
+        public function InsertUserTbl($getuserid,$getuserpassword,$getusername){
+            $pdo = $this->dbConnect();
+
+        }
+
+        //ユーザーIDでユーザー検索するメソッド
+        public function getUserTblById($getid){
+            $pdo = $this->dbConnect();
+        }
+
+        //グループを作成するメソッド
+        public function InsertGroupTbl($getgroupname,$getcategorycode,$getgrouptext){
+            $pdo = $this->dbConnect();
+        }
+
+        //グループIDでグループ検索するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+
+        //グループ参加退出テーブルをグループIDで検索するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+
+        //グループ参加退出テーブルをユーザーIDで検索するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+
+        //グループ参加退出テーブルに追加するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+
+        //グループ参加退出テーブルのデータを削除するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+
+        //チャットテーブルをグループIDで検索するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+
+        //リアクション情報をチャットIDで検索するメソッド
+        public function  (){
+            $pdo = $this->dbConnect();
+        }
+    }
 ?>
