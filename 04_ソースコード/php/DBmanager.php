f4acb6e8e6e34424d384d1f4c7eee8a15efdad15
Kakeru1011 <2101167@s.asojuku.ac.jp>
2023-06-09 12:35:54 +0900

    DBmanager修正

--- 04_ソースコード/php/DBmanager.php
+++ 04_ソースコード/php/DBmanager.php
@@ -16,13 +16,21 @@
 		    $ps = $pdo->prepare($sql);
 		    $ps->bindValue(1,$getuserid,PDO::PARAM_INT);
 		    $ps->bindValue(2,$getuserpassword,PDO::PARAM_STR);
-            $ps->bindValue(2,$getusername,PDO::PARAM_STR);
+            $ps->bindValue(3,$getusername,PDO::PARAM_STR);
 		    $ps->execute();
         }
 
         //ユーザーIDでユーザー検索するメソッド
         public function getUserTblById($getid){
             $pdo = $this->dbConnect();
+
+            $sql = "SELECT * FROM User WHERE user_id = ?";
+		    $ps = $pdo->prepare($sql);
+		    $ps->bindValue(1,$getid,PDO::PARAM_INT);
+		    $ps->execute();
+
+		    $searchArray = $ps->fetchAll();
+		    return $searchArray;
         }
 
         //グループを作成するメソッド
