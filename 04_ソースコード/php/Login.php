ca8e3de256146740e38053de0e82d4343666c857
odanihayato <114891191+odanihayato@users.noreply.github.com>
2023-06-14 09:51:04 +0900

    変更

--- 04_ソースコード/php/Login.php
+++ 04_ソースコード/php/Login.php
@@ -2,5 +2,14 @@
 session_start();
 require_once 'DBmanager.php';
 $cls = new DBManager();
-
+$searchArray = $cls->getUserTblById($_POST['userid']);
+foreach($searchArray as $row){
+    if($_POST['password'] == $row['user_password']){
+        $_SESSION['userid'] = $row['user_id'];
+        header('Location:../html/Top.html');
+      exit;
+    } else {
+    echo 'IDとパスワードが違います。'
+    }
+}
 ?>
