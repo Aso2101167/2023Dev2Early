0b3f53c582879dfadc26f692bc3231779e2423a5
Kakeru1011 <2101167@s.asojuku.ac.jp>
2023-07-12 13:09:45 +0900

    チャット機能修正

--- 04_ソースコード/javascript/Main.js
+++ 04_ソースコード/javascript/Main.js
@@ -15,6 +15,25 @@ var Main = {
             $(this).val('');
           }
         });
-    }
+    },
   
-  };
+  
+  // イベントハンドラを登録
+addEventListener: function () {
+  $('#message').bind("keydown", function (e) {
+    if (e.which == 13 && $(this).val() != '') {
+      ChatController.send($(this).val());
+      $(this).val('');
+    }
+  });
+
+  // 送信ボタンをクリックしたときの処理
+  $('button').click(function () {
+    var message = $('#message').val();
+    if (message != '') {
+      ChatController.send(message);
+      $('#message').val('');
+    }
+  });
+}
+};
